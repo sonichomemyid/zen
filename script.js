daed8454417fe4f934faf808687fdc1ea9226ba2
sonichomemyid <sonichome2026@gmail.com>
2026-08-03 23:45:50 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const sidebar = document.getElementById('sidebar');
   const overlay = document.getElementById('overlay');
   const hamburgerBtn = document.getElementById('hamburgerBtn');
-  conts closeBtn = document.getElementById('closeBtn');
+  const closeBtn = document.getElementById('closeBtn');
   
   function toggleSidebar() {
     sidebar.classList.toggle('active');
